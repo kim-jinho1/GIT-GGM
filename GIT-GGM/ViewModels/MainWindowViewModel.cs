57b5cb714fdb94ae9aad1bd2b6b17661a6d4bdde
kimjinho <russiakim2008@gmail.com>
2025-11-24 19:01:39 +0900

    fix : change project
    fix winform -> avalonia

---
+++ GIT-GGM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GIT_GGM.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ GIT-GGM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GIT_GGM.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
